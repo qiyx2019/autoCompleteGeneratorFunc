bd237dbd89ad889b3a67dedf9ed25c961481fda3
qiyx <51813543+qiyx2019@users.noreply.github.com>
2022-06-06 22:04:49 +0800

    example

--- example/test.js
+++ example/test.js
@@ -1,1 +1,9 @@
-console.log('updating...')
+const { autoCompleteGenFunc } = requier('../index.js');
+ 
+function *gen() {
+    yield 1;
+}
+const getData = () => {
+    return autoCompleteGenFunc(gen);
+}
+console.log(getData)
